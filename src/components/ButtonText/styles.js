cb740f74410f4b441fc83a410e8b054e24f24fc9
Possomato <gabrielb.possomato@gmail.com>
2025-03-20 13:48:26 -0300

    add - button text

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background-color: none;
+  color: ${({theme}) => theme.COLORS.BLUE_300};
+
+  border: none;
+  font-size: 16px;
+`
