b81f3a2819aa7b05cc181beb0dcb73f481458ee9
Possomato <gabrielb.possomato@gmail.com>
2025-04-04 13:28:25 -0300

    add - localstorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
 import { api } from '../services/api'
 
@@ -12,6 +12,9 @@ function AuthProvider({ children }) {
       const response = await api.post('/sessions', { email, password })
       const { user, token } = response.data
 
+      localStorage.setItem('@lumine:user', JSON.stringify(user))
+      localStorage.setItem('@lumine:token', token)
+
       api.defaults.headers.authorization = `Bearer ${token}`
       setData({ user, token })
     } catch (error) {
@@ -22,6 +25,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const user = localStorage.getItem('@lumine:user')
+    const token = localStorage.getItem('@lumine:token')
+
+    if(token && user){
+      api.defaults.headers.authorization = `Bearer ${token}`
+    }
+
+    setData({
+      token,
+      user: JSON.parse(user)
+    })
+  }, [])
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
