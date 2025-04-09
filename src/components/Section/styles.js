efc9238539c1773c69d94e4bdbac8ccd4fb71b37
Possomato <gabrielb.possomato@gmail.com>
2025-04-09 13:48:41 -0300

    upt - notes in feed page

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  margin: 24px 0;
+  margin: 24px 0px;
 
   >h2{
     color: ${({theme}) => theme.COLORS.WHITE};
@@ -14,8 +14,29 @@ export const Container = styled.section`
     opacity: .5;
 
     padding-bottom: 8px;
-    margin-bottom: 24px;
+    margin-bottom: 12px;
     font-size: 20px;
     font-family: "Libre Baskerville", serif;
   }
+
+  >ul{
+    list-style-type: none;
+    padding-left: 0; 
+  }
+
+  > li, a{
+    color: ${({theme}) => theme.COLORS.WHITE};
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    display: inline-block; 
+    white-space: nowrap; 
+    overflow: hidden; 
+    text-overflow: ellipsis; 
+    max-width: 100%; 
+
+    &:hover{
+    color: ${({theme}) => theme.COLORS.BLUE_300};
+  }
+  }
+
 `
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,46 +1,81 @@
+import { useState, useEffect } from 'react'
 import { Container, Content } from './styles.js'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { Button } from '../../components/Button/index.jsx'
 import { Header } from '../../components/Header/index.jsx'
 import { Section } from '../../components/Section/index.jsx'
 import { Tag } from '../../components/Tag/index.jsx'
+import { api } from '../../services/api.js'
 
 export function Details() {
+  const [data, setData] = useState(null)
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  async function handleRemove() {
+    const confirm = window.confirm('Confirma a deleção da nota?')
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+      navigate('/')
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
+
+  const formatDate = (dateString) => {
+    if (!dateString) return 'Sem data'
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' }
+    const date = new Date(dateString)
+    return date.toLocaleDateString('pt-BR', options)
+  }
+
   return (
     <Container>
       <Header />
-      <main>
-        <Content>
-          <h1>Crítica da razão pura</h1>
-          <span>25/05/2025</span>
-          <p>
-            Kant questiona os limites do conhecimento humano e a relação entre
-            experiência e razão, propondo que todo conhecimento começa com a
-            experiência, mas não deriva inteiramente dela. Para ele, o sujeito
-            cognoscente possui estruturas a priori — como espaço e tempo — que
-            organizam os dados sensíveis e tornam o conhecimento possível. Essas
-            formas de percepção e categorias do entendimento atuam como filtros
-            que moldam a maneira como o ser humano apreende o mundo, permitindo
-            que os fenômenos sejam conhecidos. No entanto, essas estruturas só
-            permitem acessar os fenômenos, ou seja, as aparências das coisas,
-            enquanto as “coisas em si”, a essência última dos objetos,
-            permanecem inacessíveis à razão humana.{' '}
-          </p>
-          <Section title="Referências">
-            <ul>
-              <li>teste1</li>
-              <li>teste2</li>
-            </ul>
-          </Section>
-          <Section title="Tags">
-            <Tag title="Kant" />
-          </Section>
-          <div className="buttons">
-            <Button title="Excluir" excluir={true} />
-            <Button title="Editar" excluir={false} />
-          </div>
-        </Content>
-      </main>
+      {data && (
+        <main>
+          <Content>
+            <h1>{data.title}</h1>
+            <span>{formatDate(data.updated_at)}</span>
+            <p>{data.description}</p>
+            {data.links && (
+              <Section title="Referências">
+                <ul>
+                  {data.links.map((link) => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </Section>
+            )}
+            {data.tags && (
+              <Section title="Tags">
+                {data.tags.map((tag) => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
+            <div className="buttons">
+              <Button title="Excluir" excluir={true} onClick={handleRemove} />
+              <Button title="Editar" excluir={false} />
+            </div>
+          </Content>
+        </main>
+      )}
     </Container>
   )
 }
