5dcb55dfd9c5ffd140c83545825b7a39ad5a8d00
Possomato <gabrielb.possomato@gmail.com>
2025-04-11 14:04:17 -0300

    fxd - vercel analtcs

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 
-import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './styles/global'
-
-import theme from './styles/theme'
-
-import { AuthProvider } from './hooks/auth.jsx'
-
-import { Routes } from './routes'
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+import { AuthProvider } from './hooks/auth.jsx';
+import { Routes } from './routes';
 
 createRoot(document.getElementById('root')).render(
-  <Analytics>
-    <StrictMode>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-      </ThemeProvider>
-    </StrictMode>
-  </Analytics>
-)
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
+    <Analytics /> 
+  </StrictMode>
+);
